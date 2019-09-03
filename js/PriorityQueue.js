e6ab51acd8f02799689f3e9209b736565fa990bf
Andrew Mushel <almushel@gmail.com>
2019-09-03 17:06:06 -0500

    Added lowest priority methods to PriorityQueue.

--- js/PriorityQueue.js
+++ js/PriorityQueue.js
@@ -22,15 +22,35 @@ class PriorityQueue {
         return priorityElement;
     }
 
+    pullLowestPriority() {
+        let lowest = this.peakLowestPriority(),
+            priorityElement = this.elements[lowest];
+        
+        this.deleteElement(lowest);
+
+        return priorityElement;
+    }
+
     peakHighestPriority() {
         let highest = 0;
-        for (let i = 0; i < this.elements.length; i++) {
+        for (let i = 0; i < this.priorities.length; i++) {
             if (this.priorities[i] > priorities[highest]) {
                 highest = i;
             }
         }
 
         return highest;
+    }
+
+    peakLowestPriority() {
+        let lowest = 0;
+        for (let i = 0; i < this.priorities.length; i++) {
+            if (this.priorities[i] < priorities[lowest]) {
+                lowest = i;
+            }
+        }
+
+        return lowest;
     }
 
     deleteElement(index) {
